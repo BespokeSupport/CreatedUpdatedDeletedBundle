6987dac6288ed613d1f6c61067caab3928d90897
Richard Seymour <web@bespoke.support>
2015-06-05 21:29:03 +0100

    remove dead files

--- build/app/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-$loader = require_once __DIR__ . '/../../vendor/autoload.php';
-
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
